6391a22efdcf29c78e53b61577d68e619310ee2f
tanavposwal <tanavposwal939@gmail.com>
2025-04-06 10:58:32 +0530

    posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "./components/nav";
 import Image from "next/image";
 import NextTopLoader from "nextjs-toploader";
 import { GeistSans } from "geist/font/sans";
+import { PostHogProvider } from "./components/PostHogProvider";
 
 export const metadata: Metadata = {
   title: {
@@ -39,25 +40,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={
-          "min-h-screen antialiased max-w-xl mx-auto " + GeistSans.className
-        }>
-        <NextTopLoader showSpinner={false} color="#242423" />
-        <Navbar />
-        <main className="flex-auto flex flex-col px-4 min-h-[80vh]">
-          {children}
-        </main>
-        <footer className="w-full flex items-center justify-center divide-x-2 divide-neutral-700 select-none">
-          <Image
-            src="/images/initial.png"
-            className="opacity-50 select-none invert brightness-100"
-            draggable={false}
-            width={150}
-            height={150}
-            alt="signature"
-          />
-        </footer>
+      <body className={"min-h-screen antialiased max-w-xl mx-auto " + GeistSans.className}>
+        <PostHogProvider>
+          <NextTopLoader showSpinner={false} color="#242423" />
+          <Navbar />
+          <main className="flex-auto flex flex-col px-4 min-h-[80vh]">
+            {children}
+          </main>
+          <footer className="w-full flex items-center justify-center divide-x-2 divide-neutral-700 select-none">
+            <Image
+              src="/images/initial.png"
+              className="opacity-50 select-none invert brightness-100"
+              draggable={false}
+              width={150}
+              height={150}
+              alt="signature"
+            />
+          </footer>
+        </PostHogProvider>
       </body>
     </html>
   );
